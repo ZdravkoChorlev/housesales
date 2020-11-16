bced735e1843dc37c76ced583ad395368130d78e
ZdravkoChorlev <softuni7z@gmail.com>
2020-11-16 22:58:29 +0200

    Add configuration for PostgreSQL

--- housesales/settings.py
+++ housesales/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'housesales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'housesales',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
